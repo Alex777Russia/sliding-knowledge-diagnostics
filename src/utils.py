5c82ffb30231b74f8a3f5d34d8120af015c2e6f8
Алексей Шевченко <admitrshevchenko@sberbank.ru>
2025-09-09 00:48:49 +0300

    simplification api and functionalities

--- src/utils.py
+++ src/utils.py
@@ -4,11 +4,6 @@ from enum import Enum
 from typing import Any, List, Dict, Optional, Union
 import uuid
 
-from src.messages_utils import Message, parse_data
-from src.quality_manager.quality_result import QMResponse
-from src.sources import Passage, Chapter, ChapterType, FAQSource, PassageSource
-from src.quality_manager.utils import TaskStatus
-
 
 @dataclass
 class StudentRequest:
